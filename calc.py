71a9e545e69c3130bc538b0cc9b9846a0e33e7b6
Aryam Sharma <aryamsharma@X.local>
2019-05-06 18:32:51 -0400

    added some functions

--- calc.py
+++ calc.py
@@ -3,4 +3,13 @@ import collections
 
 Button = collections.namedtuple('Button', ['label', 'x', 'y', 'length', 'height', 'color'])
 
+def make_buttons():
+    pass
 
+def add_buttons(buttons):
+    pass
+
+def game_loop():
+
+if __name__ == '__main__':
+    pass
